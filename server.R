f4529ba28abd84113daf61fdaf06f6bf2b5ef5fc
eldonprince <eldonprince@gmail.com>
2016-04-16 21:03:44 -0500

    Basic functionality

--- server.R
+++ server.R
@@ -7,10 +7,38 @@
 
 library(shiny)
 library(readr)
+library(ggvis)
 
 shinyServer(function(input, output) {
   
-  shinyServer(function(input) {
-    data <- read_csv(input)
+  my_data <- reactive({  
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    data.frame(read_csv(inFile$datapath))
+  })
+  
+  output$plot <- reactive({
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    dataset <- my_data()
+    suppressMessages(dataset %>% ggvis(prop("x", as.name(input$x_col)), prop("y", as.name(input$y_col))) %>% 
+      add_axis("x", title = ifelse(is.null(input$x_col), "x-axis title", input$x_col), title_offset = 75) %>%
+      add_axis("y", title = ifelse(is.null(input$y_col), "y-axis title", input$y_col), title_offset = 75) %>%
+      bind_shiny("plot"))
+  })
+               
+  output$contents <- renderTable({
+    my_data()
+  })
+
+  output$map_plot <- renderPlot({
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    dataset <- my_data()
+    plot(dataset$SQFT)
   })
 })
+
--- ui.R
+++ ui.R
@@ -1,23 +1,22 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
+library(ggvis)
 library(shiny)
+library(readr)
+redfin_names <- sort(names(read_csv("redfin_names.csv")))
 
 shinyUI(
-  pageWithSidebar(
-  
-  # Application title
-  headerPanel("Visualize Redfin Data"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      fileInput('file1', 'Choose file to upload',
-                accept = c('.csv')
-      )
+  fluidPage(
+    
+    titlePanel("Visualize Redfin Data"),
+    fileInput('file1', 'Choose CSV file from Redfin.com',
+              accept=c('text/csv', 
+                       'text/comma-separated-values,text/plain', 
+                       '.csv')),
+    selectInput('x_col', label = "x-axis", choices = redfin_names),
+    selectInput('y_col', label = "y-axis", choices = redfin_names),
+    tabsetPanel(type = "tabs", 
+                tabPanel("Plot", ggvisOutput("plot")),
+                tabPanel("Map", plotOutput("map_plot")),
+                tabPanel("Data", tableOutput("contents"))
     )
   )
 )
